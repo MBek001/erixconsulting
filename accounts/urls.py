776d3649bf727d1244aaf3bb209aaffacec9a333
user <user@kali.user>
2024-09-06 22:08:54 +0500

    fixed jala

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView
+from .views import *
+urlpatterns = [
+    path('index_en', index_en, name='index'),
+    path('index_ru', index_ru, name='index'),
+    path('index_uz', index_uz, name='index'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import render, redirect
+from django.views import View
+from django.db.models import Q
 
-# Create your views here.
+def index_en(request):
+    return render(request, 'index_en.html')
+
+def index_ru(request):
+    return render(request, 'index_ru.html')
+
+def index_uz(request):
+    return render(request, 'index_uz.html')
