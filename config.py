55395c9f0ee4231d6e185def1d8b7350e806708f
Mirzabek <124301797+MBek001@users.noreply.github.com>
2024-10-20 16:50:36 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 # DJANGO_SAVE_URL = 'http://localhost:8000/save-message'
 # CHECK_CHAT_STATUS_URL = 'http://localhost:8000/check-chat-status/'
 ### For python anywhere
-DJANGO_SAVE_URL = 'https://tuya.pythonanywhere.com/save-message'
-CHECK_CHAT_STATUS_URL = 'https://tuya.pythonanywhere.com/check-chat-status/'
+DJANGO_SAVE_URL = 'https:****'
+CHECK_CHAT_STATUS_URL = 'https:****'
 
-BOT_TOKEN='7433799472:AAEnVL0dLf73m2xUlyMzz_DuNl4aeiwAhEc'
-INFO_CHANNEL_ID='-1002413875198'
-CHANNEL_INFO_BOT='7444959956:AAGoKg3nC4ouz52Kbitb6wT4QVWov3wapAg'
-TELEGRAM_API_URL = 'https://api.telegram.org/bot7925201164:AAH_9eHCfFIxPv6SaivvSoBY9SVUB0LFF3g/sendMessage'
+BOT_TOKEN='***'
+INFO_CHANNEL_ID='****'
+CHANNEL_INFO_BOT='****'
+TELEGRAM_API_URL = 'https:***'
